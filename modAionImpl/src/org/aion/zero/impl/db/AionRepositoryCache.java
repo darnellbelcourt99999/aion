a27753776af90ff8f7e528ca632ba7eb915af2ab
Alexandra Roatis <alexandra@nuco.io>
2020-02-20 16:11:20 -0500

    Using flushTo in ExternalStateForPrecompiled

--- modAionImpl/src/org/aion/zero/impl/db/AionRepositoryCache.java
+++ modAionImpl/src/org/aion/zero/impl/db/AionRepositoryCache.java
@@ -59,6 +59,11 @@ public final class AionRepositoryCache implements RepositoryCache<AccountState> 
     }
 
     @Override
+    public Repository getParent() {
+        return this.repository;
+    }
+
+    @Override
     public void createAccount(AionAddress address) {
         lock.lock();
         try {
